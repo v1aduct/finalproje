fc30d86724ead09315eec62ce83c5ee01e3540ec
Akif Akbulut <akifakb@gmail.com>
2024-06-12 14:40:30 +0300

    Added hasta class

---
+++ hasta.py
@@ -0,0 +1,38 @@
+class Hasta:
+    def __init__(self, hasta_no, ad, soyad, dogum_tarihi, hastalik, tedavi):
+        self._hasta_no = hasta_no
+        self._ad = ad
+        self._soyad = soyad
+        self._dogum_tarihi = dogum_tarihi
+        self._hastalik = hastalik
+        self._tedavi = tedavi
+
+    def get_hasta_no(self):
+        return self._hasta_no
+    def get_ad(self):
+        return self._ad
+    def get_soyad(self):
+        return self._soyad
+    def get_dogum_tarihi(self):
+        return self._dogum_tarihi
+    def get_hastalik(self):
+        return self._hastalik
+    def get_tedavi(self):
+        return self._tedavi
+    def set_hasta_no(self, girdi):
+        self._hasta_no = girdi
+    def set_ad(self, girdi):
+        self._ad = girdi
+    def set_soyad(self, girdi):
+        self._soyad = girdi
+    def set_dogum_tarihi(self, girdi):
+        self._dogum_tarihi = girdi
+    def set_hastalik(self, girdi):
+        self._hastalik = girdi
+    def set_tedavi(self, girdi):
+        self._tedavi = girdi
+
+
+    def __str__(self):
+        return (f"Ad: {self._ad}, Soyad: {self._soyad}, Hasta No: {self._hasta_no}, "
+                f"Dogum Tarihi: {self._dogum_tarihi}, Hastalik: {self._hastalik}, Tedavi: {self._tedavi}")
